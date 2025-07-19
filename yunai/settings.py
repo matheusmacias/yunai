f9c864ee237a905787511261cc254369cec8cd4e
matheusmacias <matheusmacias3@gmail.com>
2025-07-19 01:00:49 -0300

    feat: add models

--- yunai/settings.py
+++ yunai/settings.py
@@ -22,7 +22,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_htmx",
     "core",
+    "transcription",
+    "account",
 ]
 
 MIDDLEWARE = [
@@ -33,6 +36,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 ROOT_URLCONF = "yunai.urls"
--- yunai/urls.py
+++ yunai/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("", include("core.urls")),
+    path("transcription/", include("transcription.urls")),
     path("admin/", admin.site.urls),
 ]
